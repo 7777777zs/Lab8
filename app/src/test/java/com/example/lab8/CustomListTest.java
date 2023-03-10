126cf425be39490232a39fc59f57b59a4198750e
7777777zs <xye1@ualberta.ca>
2023-03-10 15:02:39 -0700

    test countCities without countCities

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -67,15 +67,15 @@ public class CustomListTest {
         assertThrows( IllegalArgumentException.class, () -> {
             cityList.deleteCity(city); });
     }
-//    @Test
-//    void testCount() {
-//        CustomList cityList = mockCityList();
-//        assertEquals(1, cityList.countCities());
-//        City city = new City("Charlottetown", "Prince Edward Island");
-//        cityList.addCity(city);
-//        assertEquals(2, cityList.countCities());
-//        cityList.deleteCity(city);
-//        assertEquals(1, cityList.countCities());
-//
-//    }
+    @Test
+    void testCount() {
+        CustomList cityList = mockCityList();
+        assertEquals(1, cityList.countCities());
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.addCity(city);
+        assertEquals(2, cityList.countCities());
+        cityList.deleteCity(city);
+        assertEquals(1, cityList.countCities());
+
+    }
 }
